7328460d1956d6beb1be07f01616e75ae0b5d5b9
Amir Boshra <aboshra@asu.edu>
2026-03-07 16:09:30 -0700

    Feature 3: Add edge API with duplicate edge checking

--- src/main/java/edu/asu/cse464/Main.java
+++ src/main/java/edu/asu/cse464/Main.java
@@ -6,10 +6,15 @@ public class Main {
             Graph graph = GraphParser.parseGraph("input.dot");
 
             graph.addNode("x");
-            graph.addNode("a"); // duplicate, should not be added again
+            graph.addNode("a"); // duplicate node
 
             String[] newNodes = {"y", "z", "x", "m"};
             graph.addNodes(newNodes);
+
+            graph.addEdge("x", "y");
+            graph.addEdge("y", "z");
+            graph.addEdge("a", "b"); // duplicate edge, should not be added again
+            graph.addEdge("m", "a");
 
             System.out.println(graph);
 
